01780e78dfc053d00fb137f51858d407cb56395b
Ryan Andrews <127296519+Randrews1331@users.noreply.github.com>
2024-12-16 10:58:12 +0000

    Support Page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,24 +1,27 @@
-"user client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export function Header(){
-    return <div className="bg-gray-100 border-b py-6">
-        <div className="container mx-auto flex justify-between items center">
-            <Button variant="link" asChild>
-                <Link href="">AML Library</Link>
-            </Button>
-            <div>
-            <Button variant="link" asChild>
-                <Link
-                 href="/Explore">Explore</Link>
-            </Button>
+export function Header() {
+    return (
+        <div className="bg-gray-100 border-b py-6">
+            <div className="container mx-auto flex justify-between items-center">
+                <Button variant="link" asChild>
+                    <Link href="/">AML Library</Link>
+                </Button>
+                <div>
+                    <Button variant="link" asChild>
+                        <Link href="/Explore">Explore</Link>
+                    </Button>
+                </div>
+                <div>
+                    <Button variant="link" asChild>
+                        <Link href="/Support">Support</Link>
+                    </Button>
+                </div>
+                <div>Login</div>
             </div>
-            <div>Support</div>
-            <div>Login</div>
-
         </div>
-
-    </div>
+    );
 }
