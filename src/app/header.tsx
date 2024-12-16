fe04ff3d0baaadc54c8fe6a1cbbd65b937f0da28
c1023738 <c1023738@my.shu.ac.uk>
2024-12-16 16:46:29 +0000

    Reserve IDK

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,24 +1,23 @@
-"user client"
+"user client";
 
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export function Header(){
-    return <div className="bg-gray-100 border-b py-6">
-        <div className="container mx-auto flex justify-between items center">
-            <Button variant="link" asChild>
-                <Link href="">AML Library</Link>
-            </Button>
-            <div>
-            <Button variant="link" asChild>
-                <Link
-                 href="/Explore">Explore</Link>
-            </Button>
-            </div>
-            <div>Support</div>
-            <div>Login</div>
-
+export function Header() {
+  return (
+    <div className="bg-gray-100 border-b py-6">
+      <div className="container mx-auto flex justify-between items center">
+        <Button variant="link" asChild>
+          <Link href="">AML Library</Link>
+        </Button>
+        <div>
+          <Button variant="link" asChild>
+            <Link href="/Explore">Explore</Link>
+          </Button>
         </div>
-
+        <div>Support</div>
+        <div>Login</div>
+      </div>
     </div>
+  );
 }
--- src/lib/wix.ts
+++ src/lib/wix.ts
@@ -1,14 +1,14 @@
 import { createClient, OAuthStrategy } from "@wix/sdk";
-import {items} from '@wix/data';
+import { items } from "@wix/data";
 const client = createClient({
-    modules:{items},
-    auth: OAuthStrategy({
-        clientId: process.env.WIX_CLIENT_ID!,
-    }),
+  modules: { items },
+  auth: OAuthStrategy({
+    clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
+  }),
 });
 
 export function convertWixImageToUrl(wixImageUrl: string) {
-    return `https://static.wixstatic.com/media/${wixImageUrl.split("/")[3]}`;
+  return `https://static.wixstatic.com/media/${wixImageUrl.split("/")[3]}`;
 }
 
 export default client;
